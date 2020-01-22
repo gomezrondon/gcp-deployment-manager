45b74975656d6c0d4bf61a60b4ae3705c9487780
cajgomez <Jgomez@cambridgeassociates.com>
2020-01-22 13:47:30 -0600

    Adding firewall rules.

--- terraform/virtual_machine/main.tf
+++ terraform/virtual_machine/main.tf
@@ -1,24 +1,36 @@
-
-// CONDITION ? true : false
-
-variable "env" {default = "production"}
-
 resource "google_compute_instance" "default" {
   count = 1
   name = "list-${var.name_count[count.index]}"
   machine_type = var.machine_type["dev"]
   zone         = var.zone
+  can_ip_forward = false
+  description = "This is our virtual machine"
+
+  tags = ["allow-http","allow-https"] # Firewall tags
 
   boot_disk {
     initialize_params {
       image = var.image
+      size = 10
     }
+  }
+
+  labels = {
+    l_name = "list-${var.name_count[count.index]}"
+    l_machine_type = var.machine_type["dev"]
   }
 
   network_interface {
     network = "default"
 
   }
+
+  metadata = {
+    size = "10"
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo Hi > test.txt"
 
   service_account {
     scopes = ["userinfo-email", "compute-ro","storage-ro"]
