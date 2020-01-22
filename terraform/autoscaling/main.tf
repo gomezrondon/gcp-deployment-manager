fecfb02bc189d7840696fa4dd53a32c3813acee6
cajgomez <Jgomez@cambridgeassociates.com>
2020-01-22 16:43:53 -0600

    Creating an Instance Group template.

--- terraform/autoscaling/main.tf
+++ terraform/autoscaling/main.tf
@@ -1,15 +1,20 @@
-# creacion del instance template
+
+
+# Instance Template <- Describe Instance
+
 resource "google_compute_instance_template" "instance_template" {
-  name = "udemy-server-${count.index+1}}"
-  description = "this is a template"
-  #tags = [] #getworking
-  labels ={
-    enviroment = "production"
-    name = "udemy-server-${count.index+1}}"
+  name = "udemy-server-template"
+  description = "This is our autoscaling template"
+  #tags = [] # network
+
+  labels = {
+    environment = "production"
+    name = "udemy-server"
   }
-  instance_description = "this is an isntance that has been auto scaled"
-  machine_type = "f1-micro"
-  can_ip_forward = false
+
+  instance_description = "This is an instance that has been auto scaled"
+  machine_type = "n1-standard-1"
+  can_ip_forward = "false"
 
   scheduling {
     automatic_restart = true
@@ -29,13 +34,66 @@ resource "google_compute_instance_template" "instance_template" {
     type = "PERSISTENT"
   }
 
-  metadata {
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
     foo = "bar"
   }
 
   service_account {
-    scopes = ["userinfo-email","compute-ro","storage-ro"]
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
-
 }
 
+# Health Check <-- Auto Scaling Policy (when to scale )
+resource "google_compute_health_check" "health" {
+  count = 1
+  name  = "udemy"
+  check_interval_sec = 5
+  timeout_sec = 5
+  healthy_threshold = 2
+  unhealthy_threshold = 10
+
+  http_health_check {
+    request_path = "/alive.jsp"
+    port =  "8080"
+  }
+}
+
+# Group Manager < -- Manages the nodes
+resource "google_compute_region_instance_group_manager" "instance_group_manager" {
+  name = "instance-group-manager"
+  //  instance_template = "${google_compute_instance_template.instance_template.self_link}"
+  base_instance_name = "instance-group-manager"
+  region = var.region
+
+  version {
+    name = "v1-ig"
+    instance_template = "${google_compute_instance_template.instance_template.self_link}"
+  }
+
+  auto_healing_policies {
+    health_check = google_compute_health_check.health[0].self_link
+    initial_delay_sec = 300
+  }
+}
+
+# Auto Scale Policy < -- How many instances
+resource "google_compute_region_autoscaler" "autoscaler" {
+  count = 1
+  name = "autoscaler"
+  project = var.project
+  region = var.region
+  target = "${google_compute_region_instance_group_manager.instance_group_manager.self_link}"
+
+  autoscaling_policy  {
+    max_replicas = 2
+    min_replicas = 1
+    cooldown_period = 60
+    cpu_utilization {
+      target = "0.8"
+    }
+  }
+}
